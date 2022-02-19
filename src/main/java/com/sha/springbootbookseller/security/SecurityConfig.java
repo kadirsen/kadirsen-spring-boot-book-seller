f42775cba02eb64699b32c6361143cea13044afb
Kadir ŞEN <kadirsen002@gmail.com>
2022-02-19 21:04:49 +0300

    Security config bug fix

--- src/main/java/com/sha/springbootbookseller/security/SecurityConfig.java
+++ src/main/java/com/sha/springbootbookseller/security/SecurityConfig.java
@@ -24,73 +24,78 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig  extends WebSecurityConfigurerAdapter {
-
+public class SecurityConfig extends WebSecurityConfigurerAdapter
+{
     @Value("${authentication.internal-api-key}")
     private String internalApiKey;
 
     @Autowired
-    private  CustomUserDetailService userDetailService;
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    private CustomUserDetailService userDetailsService;
 
-        auth.userDetailsService(userDetailService)
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception
+    {
+        auth.userDetailsService(userDetailsService)
                 .passwordEncoder(passwordEncoder());
     }
 
-    @Bean
-    public JwtAuthorizationFilter jwtAuthorizationFilter(){
-        return new JwtAuthorizationFilter();
+    @Override
+    @Bean(BeanIds.AUTHENTICATION_MANAGER)
+    public AuthenticationManager authenticationManagerBean() throws Exception
+    {
+        return super.authenticationManagerBean();
     }
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
-
+    protected void configure(HttpSecurity http) throws Exception
+    {
         http.cors();
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
         http.authorizeRequests()
                 .antMatchers("/api/authentication/**").permitAll()
-                .antMatchers(HttpMethod.GET,"/api/book").permitAll()
+                .antMatchers(HttpMethod.GET, "/api/book").permitAll()
                 .antMatchers("/api/book/**").hasRole(Role.ADMIN.name())
-                .antMatchers("/api/internal/**").hasRole(Role.SYSTEM_MANEGER.name())
+                .antMatchers("/api/internal/**").hasRole(Role.SYSTEM_MANAGER.name())
                 .anyRequest().authenticated();
 
         //jwt filter
+        //internal > jwt > authentication
         http.addFilterBefore(jwtAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class)
-                .addFilterBefore(internalApiAuthenticationFilter(),JwtAuthorizationFilter.class);
+                .addFilterBefore(internalApiAuthenticationFilter(), JwtAuthorizationFilter.class);
     }
 
-    @Override
-    @Bean(BeanIds.AUTHENTICATION_MANAGER)
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
-    }
     @Bean
     public InternalApiAuthenticationFilter internalApiAuthenticationFilter()
     {
         return new InternalApiAuthenticationFilter(internalApiKey);
     }
 
+    @Bean
+    public JwtAuthorizationFilter jwtAuthorizationFilter()
+    {
+        return new JwtAuthorizationFilter();
+    }
 
     @Bean
-    public PasswordEncoder passwordEncoder (){
+    public PasswordEncoder passwordEncoder()
+    {
         return new BCryptPasswordEncoder();
     }
 
     @Bean
-    public WebMvcConfigurer corsConfigurer(){
-
-        return new WebMvcConfigurer() {
+    public WebMvcConfigurer corsConfigurer()
+    {
+        return new WebMvcConfigurer()
+        {
             @Override
-            public void addCorsMappings(CorsRegistry registry) {
+            public void addCorsMappings(CorsRegistry registry)
+            {
                 registry.addMapping("/**")
                         .allowedOrigins("*")
                         .allowedMethods("*");
             }
         };
-
     }
-
 }
